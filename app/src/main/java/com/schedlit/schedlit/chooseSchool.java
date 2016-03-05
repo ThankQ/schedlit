93c21e5537570446a10921d1a35f628f484ce622
ThankQ <qhuyett1@umbc.edu>
2016-03-05 13:58:52 -0500

    Added a chooseSchool page

--- app/src/main/java/com/schedlit/schedlit/chooseSchool.java
+++ app/src/main/java/com/schedlit/schedlit/chooseSchool.java
@@ -3,13 +3,16 @@ package com.schedlit.schedlit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class loginPage extends AppCompatActivity {
+public class chooseSchool extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_page);
+        setContentView(R.layout.activity_choose_school);
     }
 
+    public void getSchoolList()
+    {
 
+    }
 }
