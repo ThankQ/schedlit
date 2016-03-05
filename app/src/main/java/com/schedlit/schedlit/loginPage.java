3f8af094e32d96b3704f4cef44b190d13095129a
ThankQ <qhuyett1@umbc.edu>
2016-03-05 16:43:45 -0500

    Created Firebase code to get make a list to choose the school

--- app/src/main/java/com/schedlit/schedlit/loginPage.java
+++ app/src/main/java/com/schedlit/schedlit/loginPage.java
@@ -3,13 +3,28 @@ package com.schedlit.schedlit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
 public class loginPage extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+
+
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
 
+
+    }
 }
