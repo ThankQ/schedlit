7357d4e48dc0020c1f57b0c0f7e69dfe21a3710c
emcgov1 <emcgov1@umbc.edu>
2016-03-05 14:03:07 -0500

    Login Page Design
    Designed layout of the basic login page. Created edit text fields for
    the email and password.

--- app/src/main/java/com/schedlit/schedlit/loginPage.java
+++ app/src/main/java/com/schedlit/schedlit/loginPage.java
@@ -2,8 +2,12 @@ package com.schedlit.schedlit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class loginPage extends AppCompatActivity {
+    EditText emailEditText;
+    EditText pwordEditText;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
