81d632e241217ca715226d67b74788365e46424e
ThankQ <qhuyett1@umbc.edu>
2016-03-05 13:44:04 -0500

    Wrote a comment in loginPage.java

--- app/src/main/java/com/schedlit/schedlit/loginPage.java
+++ app/src/main/java/com/schedlit/schedlit/loginPage.java
@@ -10,4 +10,6 @@ public class loginPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
     }
+    //is this is qFirebase branch?
+
 }
